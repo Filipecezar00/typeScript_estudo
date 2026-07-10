9a5651db181d86bb22ff9c32eb20710b8d85f18e
Filipecezar00 <cezarfilipe01@gmail.com>
2026-07-09 21:56:56 -0300

    Atualizando

---
+++ conceitos/exemplo1.js
@@ -0,0 +1,6 @@
+var mensagem = "Olá mundo";
+mensagem = "Outra String";
+var idade = 35;
+
+var rodando = true;
+rodando = false;
---
+++ conceitos/exemplo1.ts
@@ -0,0 +1,7 @@
+let mensagem: string = "Ola mundo";
+mensagem = "Outra String";
+
+let idade: number = 35;
+
+let rodando: boolean = true;
+rodando = false;
