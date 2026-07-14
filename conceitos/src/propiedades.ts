dddfb380462d77ad98f2091440b08400d1da6ac2
Filipecezar00 <cezarfilipe01@gmail.com>
2026-07-13 23:48:44 -0300

    atualizando

--- conceitos/src/propiedades.ts
+++ conceitos/src/propiedades.ts
@@ -1,11 +1,14 @@
 interface Individuo {
   prenome: string;
   sobrenome: string;
-  idade: number;
+  idade?: number;
 }
 function imprimirNome(individuo: Individuo) {
   const nome = individuo.prenome + " " + individuo.sobrenome;
   console.log(`Meu nome é ${nome}`);
+  if (individuo.idade) {
+    console.log(`Tenho ${individuo.idade} anos.`);
+  }
 }
 let alguem = {
   prenome: "Ana",
