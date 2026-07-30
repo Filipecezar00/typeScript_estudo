16689e289b840f6f831727197afb4dda228bdf63
Filipecezar00 <cezarfilipe01@gmail.com>
2026-07-29 23:36:44 -0300

    atualizando

---
+++ conceitos/src/individuo.ts
@@ -0,0 +1,14 @@
+class Individuo {
+  private nome: string;
+  constructor(oNome: string) {
+    this.nome = oNome;
+  }
+
+  public cumprimentar(): void {
+    console.log(`Oi, meu nome é ${this.nome}`);
+  }
+}
+
+let maria = new Individuo("Maria");
+maria.cumprimentar();
+maria.nome = "João";
