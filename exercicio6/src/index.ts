619540bf6762be2b670f7b6ed3eedb6c5c5d6836
Filipecezar00 <cezarfilipe01@gmail.com>
2026-07-30 01:10:42 -0300

    atualizando

--- exercicio6/src/index.ts
+++ exercicio6/src/index.ts
@@ -8,6 +8,15 @@ const input_valor_total = document.getElementById(
 const cupom_desconto = document.getElementById(
   "cupom_de_desconto",
 ) as HTMLInputElement;
+const form_cadastro = document.getElementById(
+  "form_cadastro",
+) as HTMLFormElement;
+const container_valor_total = document.getElementById(
+  "container_valor_total",
+) as HTMLElement;
+const historico_pedidos = document.getElementById(
+  "historico_pedidos",
+) as HTMLElement;
 
 interface PedidoDTO {
   readonly id: number;
@@ -19,7 +28,7 @@ interface PedidoDTO {
 class CaixaLoja {
   private saldoTotal: number = 0;
   public processarPedido(pedido: PedidoDTO): void {
-    let valor_total: number = Number(input_valor_total.value);
+    let valor_total: number = Number(pedido.valor_total);
 
     if (pedido.cupom) {
       valor_total = valor_total * 0.9;
@@ -30,3 +39,31 @@ class CaixaLoja {
     return this.saldoTotal;
   }
 }
+const caixa = new CaixaLoja();
+
+form_cadastro.addEventListener("submit", (e) => {
+  e.preventDefault();
+  try {
+    let id_value: number = Number(id_pedido.value);
+    let cliente_value: string = nome_cliente.value;
+    let valor_total_value: number = Number(input_valor_total.value);
+    let cupom_value =
+      cupom_desconto.value.trim() !== "" ? cupom_desconto.value : null;
+
+    let novoPedido: PedidoDTO = {
+      id: id_value,
+      nome: cliente_value,
+      valor_total: valor_total_value,
+      cupom: cupom_value,
+    };
+    caixa.processarPedido(novoPedido);
+
+    container_valor_total.innerHTML = `R$${caixa.obterSaldo().toFixed(2)}`;
+    historico_pedidos.innerHTML += `<pre>
+    Id:${novoPedido.id} Nome:${novoPedido.nome} Valor total:R$ ${novoPedido.valor_total} Cupom: ${novoPedido.cupom}
+    </pre>
+    `;
+  } catch (error) {
+    console.error("ERRO AO EXECUTAR EVENTO DO FORMULÁRIO:", error);
+  }
+});
