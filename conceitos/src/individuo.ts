b78d1e986c52448dcda56cc4ec2bc109f9f8a678
Filipecezar00 <cezarfilipe01@gmail.com>
2026-07-30 22:49:21 -0300

    atualizando

--- conceitos/src/individuo.ts
+++ conceitos/src/individuo.ts
@@ -8,8 +8,20 @@ namespace SobreClasses {
     public cumprimentar(): void {
       console.log(`Oi, meu nome é ${this.nome}`);
     }
+
+    public setNome(novoNome: string): void {
+      this.nome = novoNome;
+    }
+
+    public getNome(): string {
+      return this.nome;
+    }
   }
 
   let maria = new Individuo("Maria");
   maria.cumprimentar();
+
+  maria.setNome("Joao");
+
+  console.log("O nome do individuo é " + maria.getNome());
 }
